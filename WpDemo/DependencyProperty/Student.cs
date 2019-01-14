126359ced6f034ea4f0d1eb86aac09a609ea53cc
bshaoCN <bshao@163.com>
2019-01-14 18:26:15 +0800

    Dependency property

---
+++ WpDemo/DependencyProperty/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpDemo.DependencyProperty
+{
+    public class Student : DependencyObject
+    {
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public static readonly System.Windows.DependencyProperty NameProperty =
+            System.Windows.DependencyProperty.Register("Name", typeof(string), typeof(Student), new PropertyMetadata(new PropertyChangedCallback(OnNameChanged)));
+
+        private static void OnNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            int i;
+            i= 1;
+        }
+    }
+}
---
+++ WpDemo/DependencyProperty/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpDemo.DependencyProperty
+{
+    /// <summary>
+    /// Interaction logic for Student.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Student Student;
+        public Window1()
+        {
+            InitializeComponent();
+
+            Student = new Student();
+            BindingOperations.SetBinding(Student, Student.NameProperty,
+                new Binding("Text") { Source = name1, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged, Mode = BindingMode.TwoWay });
+            name2.SetBinding(TextBox.TextProperty,
+                new Binding("Name") { Source = Student, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged, Mode = BindingMode.TwoWay });
+
+        }
+    }
+}
